7ad5aaa8f4f4ee0e6f4ad236981de5cba850566e
Daniel Barnes <81057377+danielzbarnes@users.noreply.github.com>
2021-11-21 09:41:48 -0800

    added PodcastDetailViewModel.kt

---
+++ app/src/main/java/com/danielzbarnes/podcastapp/details/PodcastDetailViewModel.kt
@@ -0,0 +1,30 @@
+package com.danielzbarnes.podcastapp.details
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import com.danielzbarnes.podcastapp.database.Podcast
+import com.danielzbarnes.podcastapp.repository.DownloadRepository
+import com.danielzbarnes.podcastapp.repository.PodcastRepository
+
+class PodcastDetailViewModel: ViewModel() {
+
+    private val podcastRepository = PodcastRepository.get()
+    private val downloadRepository = DownloadRepository.get()
+    private val podcastIdLiveData = MutableLiveData<String>()
+
+    var podcastLiveData: LiveData<Podcast?> = Transformations.switchMap(podcastIdLiveData){
+            podcastId -> podcastRepository.getPodcast(podcastId)
+    }
+
+    fun loadPodcast(podcastId: String){
+        podcastIdLiveData.value = podcastId
+    }
+
+    fun isPodcastCached(url: String): Boolean{
+        return downloadRepository.getDownloads().contains(url)
+    }
+
+
+}
